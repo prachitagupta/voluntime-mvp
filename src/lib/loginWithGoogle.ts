08a53da5ab693846792671e32712a0da6ed65ff5
Prachita Gupta <gupta.prachita@gmail.com>
2025-08-17 21:35:34 +1000

    add redirect to vercel app dashboard

--- src/lib/loginWithGoogle.ts
+++ src/lib/loginWithGoogle.ts
@@ -4,10 +4,15 @@
 import { supabase } from './supabaseClient';
 
 export async function loginWithGoogle() {
+  const redirectTo = typeof window !== 'undefined' && window.location.origin
+    ? `${window.location.origin}/dashboard`
+    : 'https://voluntime-mvp.vercel.app/dashboard';
+
+
   await supabase.auth.signInWithOAuth({
     provider: 'google',
     options: {
-      redirectTo: `${window.location.origin}/auth/callback`,
+      redirectTo: redirectTo,
     },
   });
 }
